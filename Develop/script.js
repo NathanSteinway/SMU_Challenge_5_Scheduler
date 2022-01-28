b0a2614d2c68fb70d14ea3edbabfc57a5bdbf137
unknown <steinway.nathan@gmail.com>
2022-01-27 19:57:14 -0600

    created colorcoding function to compare time block hours to current hours using momentjs and jquery

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,31 @@ function getDate() {
     currentDateSpan.innerHTML = rightNow;
     document.getElementById("currentDay").appendChild(currentDateSpan);
 
-}
+};
+
+function colorCoding() {
+
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+
+        var irlHour = parseInt($(this).attr("id"));
+
+        if (irlHour < hour) {
+
+            $(this).addClass("past");
+
+        } else if (irlHour === hour) {
+
+            $(this).addClass("present");
+
+        } else {
+
+            $(this).addClass("future");
+
+        }
+    })
+};
 
 getDate();
+colorCoding();
